8ad94e3534dba81ef330b7e92fba2ad024406f7f
Andrew Duncan <andrew.duncan@oicr.on.ca>
2018-09-13 10:15:27 -0400

    managed to get zip and basic parsing working

--- icgc-viewer/js/Store/SeqFeature/icgcCopyNumber.js
+++ icgc-viewer/js/Store/SeqFeature/icgcCopyNumber.js
@@ -1,21 +1,59 @@
+const zlib = cjsRequire("zlib");
+const request = cjsRequire("request");
+const http = cjsRequire("http");
+
 define([
     'dojo/_base/declare',
-    'dojo/_base/array',
-    'dojo/request',
     'JBrowse/Store/SeqFeature',
     'JBrowse/Model/SimpleFeature'
 ],
 function(
     declare,
-    array,
-    request,
     SeqFeatureStore,
     SimpleFeature
 ) {
     return declare(SeqFeatureStore, {
 
+        zipPromise: undefined,
+        buffer: [],
+
         constructor: function(args) {
-            this.donor = args.donor;
+            var thisB = this;
+            this.donor = 'DO229446';
+            var url = 'https://dcc.icgc.org/api/v1/download/submit?filters={"donor":{"id":{"is":["' + this.donor + '"]}}}&info=[{"key":"cnsm","value":"JSON"}]';
+            thisB.zipPromise = fetch(url, {
+                method: 'GET'
+            }).then(function(res) {
+                return res.json()
+            }).then(function(res) {
+                var downloadId = res.downloadId;
+                var downloadLink = 'https://dcc.icgc.org/api/v1/download/' + downloadId;
+                console.log(downloadLink);
+                return new Promise((resolve, reject) => {
+                    http.get(downloadLink, function(res) {
+                        var gunzip = zlib.createGunzip();            
+                        res.pipe(gunzip);
+
+                        gunzip.on('data', function(data) {
+                            thisB.buffer.push(data.toString())
+                        }).on("end", function() {
+                            thisB.buffer = thisB.buffer.join("");               
+                        }).on("error", function(e) {
+                            console.log('error!');
+                            console.log(e);
+                            thisB.buffer = thisB.buffer.join("");
+                            resolve("failure");
+                        })
+                    }).on('error', function(e) {
+                        console.log(e)
+                        resolve("failure");
+                    });
+                })
+            }, function(err) {
+                console.log(err);
+                errorCallback('Error contacting ICGC Portal');
+            });
+
         },
 
         getFeatures: function(query, featureCallback, finishCallback, errorCallback) {
@@ -27,9 +65,45 @@ function(
             var start = query.start;
             var end = query.end;
             var ref = query.ref.replace(/chr/, '');
-            end = thisB.getChromosomeEnd(ref, end);
-            
-            finishCallback();
+
+            thisB.zipPromise.then(function(zip) {
+                if (thisB.buffer) {
+                    var isHeaderLine = true;
+                    var chr = null;
+                    var chrStart = 0;
+                    var chrEnd = 1;
+                    var segMean = null;
+
+                    var splitFile = thisB.buffer.split(/\n/);
+                    splitFile.forEach((element, index, array) => {
+                        var splitLine = element.split(/\t/);
+                        if (isHeaderLine) {
+                            chr = splitLine.indexOf("chromosome");
+                            chrStart = splitLine.indexOf("chromosome_start");
+                            chrEnd = splitLine.indexOf("chromosome_end");
+                            segMean = splitLine.indexOf("segment_mean");
+                        } else {
+                            if (splitLine[chr] === ref) {
+                                featureCallback(new SimpleFeature({
+                                    id: splitLine[chr] + "_" + splitLine[chrStart] + "_" + splitLine[chrEnd] + "_copyNumber",
+                                    data: {
+                                        start: splitLine[chrStart],
+                                        end: splitLine[chrEnd],
+                                        score: splitLine[segMean]
+                                    }
+                                }));
+                            }
+                        }
+
+                        isHeaderLine = false;
+                    });
+                }
+    
+                finishCallback();
+            }, function(err) {
+                console.log(err);
+                errorCallback(err);
+            });
         }
     });
 });
