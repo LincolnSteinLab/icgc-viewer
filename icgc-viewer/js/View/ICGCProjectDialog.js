096bf10d4a19f4587dec22e3e90a5e9dfda417cb
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-04-04 10:48:46 -0400

    use placeholder for ssms generated through dialog

--- icgc-viewer/js/View/ICGCProjectDialog.js
+++ icgc-viewer/js/View/ICGCProjectDialog.js
@@ -250,6 +250,7 @@ function (
                     }
                 );
             } else if (storeClass === 'SimpleSomaticMutations') {
+                trackConf.fmtDetailValue_projects = function(value) { return "<div id='projects-icgc-" + value +  "'></div" };
                 trackConf.menuTemplate.push(
                     {   
                         label : "Highlight this Simple Somatic Mutation",
