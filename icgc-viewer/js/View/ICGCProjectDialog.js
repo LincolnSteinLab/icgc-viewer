d687532d644c41b1fbedfc307e6452d18979755d
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-04-04 13:50:15 -0400

    loading note

--- icgc-viewer/js/View/ICGCProjectDialog.js
+++ icgc-viewer/js/View/ICGCProjectDialog.js
@@ -250,7 +250,7 @@ function (
                     }
                 );
             } else if (storeClass === 'SimpleSomaticMutations') {
-                trackConf.fmtDetailValue_projects = function(value) { return "<div id='projects-icgc-" + value +  "'></div" };
+                trackConf.fmtDetailValue_projects = function(value) { return "<div id='projects-icgc-" + value +  "'>Loading...</div" };
                 trackConf.menuTemplate.push(
                     {   
                         label : "Highlight this Simple Somatic Mutation",
