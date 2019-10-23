2679f0a22145f6bec7447f51526e5bc2815fd8ff
Andrew Duncan <andrew.duncan@oicr.on.ca>
2019-10-23 13:48:40 -0400

    fix remote tests

--- cypress/integration/GeneExport.js
+++ cypress/integration/GeneExport.js
@@ -24,7 +24,7 @@ describe('Select tracks', function() {
      * @param {Array<string>} textValues Values to check for in export
      */
     var testExport = function (radioIndex, exportType, textValues) {
-        cy.get('.track-menu-button').eq(1).click()
+        cy.get('.track-menu-button').click()
         cy.contains('Save track data').click()
         cy.contains(exportType)
         cy.get('.dijitCheckBoxInput').eq(radioIndex).click()
@@ -43,17 +43,9 @@ describe('Select tracks', function() {
         testExport(3, 'BED', ['track name="ICGC_Genes" useScore=0', '1	103342022	103574051	ENSG00000060718		-'])
         testExport(4, 'CSV', ['type,start,end,strand,id,gene name,biotype,symbol,type', 'protein_coding,103342022,103574051,-1,,collagen, type XI, alpha 1,,COL11A1,protein_coding'])
         testExport(5, 'Sequin Table', ['>Feature 1', '237205505	237997287	protein_coding'])
-        testExport(6, 'Track Config', 
-        [
-            `[tracks.ICGC_Genes]
-storeClass=icgc-viewer/Store/SeqFeature/Genes
-type=icgc-viewer/View/Track/GeneTrack
-key=ICGC_Genes
-metadata.datatype=Genes
-unsafePopup=true
-size=1000
-filters={"gene":{"location":{"is":["1:0-248899548"]}}}`
-        ])
+        cy.fixture('gene/track-conf-export.conf').then((json) => {
+            testExport(6, 'Track Config', [json])
+        })
         testExport(7, 'Track Config JSON', 
         [
 `{
@@ -72,8 +64,8 @@ filters={"gene":{"location":{"is":["1:0-248899548"]}}}`
     })
 
     it('Should be able to export an Gene track', function() {
-        cy.get('.track-menu-button').eq(1).click()
+        cy.get('.track-menu-button').click()
         cy.contains('Share Track as URL').click()
-        cy.get('textarea').should('have.value', 'http://localhost:3000/?loc=1%3A1..194440331&tracks=DNA%2CICGC_Genes&highlight=&addTracks=%5B%7B%22label%22%3A%22ICGC_Genes%22%2C%22storeClass%22%3A%22icgc-viewer%2FStore%2FSeqFeature%2FGenes%22%2C%22type%22%3A%22icgc-viewer%2FView%2FTrack%2FGeneTrack%22%2C%22key%22%3A%22ICGC_Genes%22%2C%22metadata%22%3A%7B%22datatype%22%3A%22Gene%22%7D%2C%22unsafePopup%22%3Atrue%7D%5D')
+        cy.get('textarea').should('have.value', 'http://localhost:3000/?loc=1%3A1..194440331&tracks=ICGC_Genes&highlight=&addTracks=%5B%7B%22label%22%3A%22ICGC_Genes%22%2C%22storeClass%22%3A%22icgc-viewer%2FStore%2FSeqFeature%2FGenes%22%2C%22type%22%3A%22icgc-viewer%2FView%2FTrack%2FGeneTrack%22%2C%22key%22%3A%22ICGC_Genes%22%2C%22metadata%22%3A%7B%22datatype%22%3A%22Gene%22%7D%2C%22unsafePopup%22%3Atrue%7D%5D')
     })
 })
--- cypress/integration/SSMExport.js
+++ cypress/integration/SSMExport.js
@@ -24,7 +24,7 @@ describe('Select tracks', function() {
      * @param {Array<string>} textValues Values to check for in export
      */
     var testExport = function (radioIndex, exportType, textValues) {
-        cy.get('.track-menu-button').eq(1).click()
+        cy.get('.track-menu-button').click()
         cy.contains('Save track data').click()
         cy.contains(exportType)
         cy.get('.dijitCheckBoxInput').eq(radioIndex).click()
@@ -43,17 +43,10 @@ describe('Select tracks', function() {
         testExport(3, 'BED', ['track name="ICGC_Mutations" useScore=0', '1	115256528	115256528	MU68272'])
         testExport(4, 'CSV', ['type,start,end,strand,id,allele in the reference assembly,mutation,reference genome assembly', 'single base substitution,115256528,115256528,,,T,T>C,GRCh37'])
         testExport(5, 'Sequin Table', ['>Feature 1', '115256529	115256528	single base substitution'])
-        testExport(6, 'Track Config', 
-        [
-            `[tracks.ICGC_Mutations]
-storeClass=icgc-viewer/Store/SeqFeature/SimpleSomaticMutations
-type=icgc-viewer/View/Track/SSMTrack
-key=ICGC_Mutations
-metadata.datatype=SimpleSomaticMutations
-unsafePopup=true
-size=500
-filters={"mutation":{"location":{"is":["1:0-248899548"]}}}`
-        ])
+
+        cy.fixture('ssm/track-conf-export.conf').then((json) => {
+            testExport(6, 'Track Config', [json])
+        })
         testExport(7, 'Track Config JSON', 
         [
 `{
@@ -72,8 +65,8 @@ filters={"mutation":{"location":{"is":["1:0-248899548"]}}}`
     })
 
     it('Should be able to export an SSM track', function() {
-        cy.get('.track-menu-button').eq(1).click()
+        cy.get('.track-menu-button').click()
         cy.contains('Share Track as URL').click()
-        cy.get('textarea').should('have.value', 'http://localhost:3000/?loc=1%3A1..194440331&tracks=DNA%2CICGC_Mutations&highlight=&addTracks=%5B%7B%22label%22%3A%22ICGC_Mutations%22%2C%22storeClass%22%3A%22icgc-viewer%2FStore%2FSeqFeature%2FSimpleSomaticMutations%22%2C%22type%22%3A%22icgc-viewer%2FView%2FTrack%2FSSMTrack%22%2C%22key%22%3A%22ICGC_Mutations%22%2C%22metadata%22%3A%7B%22datatype%22%3A%22SSM%22%7D%2C%22unsafePopup%22%3Atrue%7D%5D')
+        cy.get('textarea').should('have.value', 'http://localhost:3000/?loc=1%3A1..194440331&tracks=ICGC_Mutations&highlight=&addTracks=%5B%7B%22label%22%3A%22ICGC_Mutations%22%2C%22storeClass%22%3A%22icgc-viewer%2FStore%2FSeqFeature%2FSimpleSomaticMutations%22%2C%22type%22%3A%22icgc-viewer%2FView%2FTrack%2FSSMTrack%22%2C%22key%22%3A%22ICGC_Mutations%22%2C%22metadata%22%3A%7B%22datatype%22%3A%22SSM%22%7D%2C%22unsafePopup%22%3Atrue%7D%5D')
     })
 })
