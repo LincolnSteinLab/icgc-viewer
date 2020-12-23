23f494f4b75b4b7fef1f93721c87b4967a2b5e9c
Gregory Hogue <gfjhogue@gmail.com>
2020-12-23 17:26:10 -0500

    adopt login/token code from gdc-viewer for ICGC token auth

--- icgc-viewer/js/main.js
+++ icgc-viewer/js/main.js
@@ -2,35 +2,58 @@ define([
         'dojo/_base/declare',
         'dojo/_base/lang',
         'JBrowse/Plugin',
+        'dijit/Menu',
         'dijit/MenuItem',
+        'dijit/PopupMenuItem',
         './View/ICGCDialog',
-        './View/ICGCProjectDialog'
+        './View/ICGCProjectDialog',
+        './View/ICGCStoreTokenDialog'
        ],
        function(
             declare,
             lang,
             JBrowsePlugin,
+            Menu,
             MenuItem,
+            PopupMenuItem,
             ICGCDialog,
-            ICGCProjectDialog
+            ICGCProjectDialog,
+            ICGCStoreTokenDialog
        ) {
 return declare(JBrowsePlugin, {
         constructor: function () {
             this.browser.afterMilestone('initView', function () {
-                this.browser.addGlobalMenuItem('icgc', new MenuItem(
-                    {
-                        label: 'Explore donors, genes and mutations',
-                        iconClass: "dijitIconSearch",
-                        onClick: lang.hitch(this, 'createICGCTrack')
-                    }));
-                this.browser.addGlobalMenuItem('icgc', new MenuItem(
-                    {
-                        label: 'Explore projects',
-                        iconClass: "dijitIconSearch",
-                        onClick: lang.hitch(this, 'createICGCProjectTrack')
-                    }));
-                    this.browser.renderGlobalMenu('icgc', {text: 'ICGC'}, this.browser.menuBar);
-            }, this);            
+                var explorationSubmenu = new Menu();
+                explorationSubmenu.addChild(new MenuItem({
+                    label: 'Explore donors, genes and mutations',
+                    iconClass: "dijitIconSearch",
+                    onClick: lang.hitch(this, 'createICGCTrack')
+                }));
+                explorationSubmenu.addChild(new MenuItem({
+                    label: 'Explore projects',
+                    iconClass: "dijitIconSearch",
+                    onClick: lang.hitch(this, 'createICGCProjectTrack')
+                }));
+                this.browser.addGlobalMenuItem('icgc', new PopupMenuItem({
+                    label: "Exploration",
+                    iconClass: "dijitIconSearch",
+                    popup: explorationSubmenu
+                }));
+
+                var fileSubmenu = new Menu();
+                fileSubmenu.addChild(new MenuItem({
+                    label: "Login",
+                    iconClass: "dijitIconUsers",
+                    onClick: lang.hitch(this, 'createICGCLogin')
+                }));
+                this.browser.addGlobalMenuItem('icgc', new PopupMenuItem({
+                    label: "Repository files",
+                    iconClass: "dijitIconFile",
+                    popup: fileSubmenu
+                }));
+
+                this.browser.renderGlobalMenu('icgc', {text: 'ICGC'}, this.browser.menuBar);
+            }, this);
         },
 
         createICGCTrack: function () {
@@ -59,6 +82,15 @@ return declare(JBrowsePlugin, {
                 function () {
 
                 });
+        },
+
+        createICGCLogin: function() {
+            var searchDialog = new ICGCStoreTokenDialog({
+                onHide: function() {
+                    this.destroy();
+                }
+            });
+            searchDialog.show(this.browser, function(){});
         }
     });
 });
